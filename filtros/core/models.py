d4c4d439d97d02d007e7d83e0ae4b88460d70ac5
Tavares <lucas.tavares1996@hotmail.com>
2019-04-19 14:09:58 -0300

    Arrumando a verbose_name da app CORE

--- filtros/core/models.py
+++ filtros/core/models.py
@@ -4,6 +4,10 @@ from django.db import models
 class Autor(models.Model):
     name = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+
     def __str__(self):
         return self.name
 
@@ -22,6 +26,10 @@ class Jornal(models.Model):
     data_publicada = models.DateTimeField(auto_now_add=True)
     visualizacao = models.IntegerField(default=0)
     revisado = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = 'Jornal'
+        verbose_name_plural = 'Jornais'
 
     def __str__(self):
         return self.name
